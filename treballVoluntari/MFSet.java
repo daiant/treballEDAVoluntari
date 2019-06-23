980d9edd62edb6b5c2291ee7bb3031b722aaffd2
SHEKK0 <39417075+SHEKK0@users.noreply.github.com>
2019-06-23 10:40:57 +0200

    Add files via upload

---
+++ treballVoluntari/MFSet.java
@@ -0,0 +1,54 @@
+package treballVoluntari;
+
+public class MFSet {
+    int [] mfset;
+
+    public MFSet(int numElem){
+    mfset = new int[numElem];
+    for (int i = 0; i < numElem; i++) {
+        mfset[i]=-1;
+    }
+    }
+
+    public int find(int pos){
+    if (pos < 0 || pos >= mfset.length){
+        return -1; // no tÃ© representant
+    }
+    while (mfset[pos] > 0 ){
+        pos = mfset[pos];
+    }
+    return pos; 
+    }
+
+    public boolean merge(int elem1, int elem2){
+        int rep1 = find(elem1);
+	int rep2 = find(elem2);
+
+	if (rep1 <0 || rep2 <0) return false;
+
+	if (rep1 != rep2){
+	    if (mfset[rep1] <  mfset[rep2]) { //el arbre primer es mes gran
+		mfset[rep2] = rep1;
+	    } else {
+		mfset[rep1] = rep2;
+		if (mfset[rep1] == mfset[rep2]){
+		    mfset[rep2]--;
+		}
+	    }
+	}
+	return true;
+    }
+    
+    public int findCardinalitat(int x){
+        int res = 1;
+        int num = find(x);
+        for(int i = 0; i < mfset.length -1; i++){
+            if(mfset[i] == num){
+                res += findCardinalitat(x);
+            }
+        }
+        return res;
+        // int sizeX = find(x) * -1; // Número de pixeles en el conjunto, no?
+        // return Math.abs(sizeX); // Pues al revés
+    }
+}
---
+++ treballVoluntari/ProcessatImages.java
@@ -0,0 +1,54 @@
+package treballVoluntari;
+
+public class ProcessatImages{
+    protected int [][] img;
+    protected int llindar;
+
+    public ProcessatImages(int [][] img, int llindar){
+    this.img=img;
+    this.llindar=llindar;
+    }
+
+    
+    //Passa un pixel en forma de (fila,columna) a la un int
+    public int pixel2Int(int f, int c){
+    return f*img[f].length + c;
+    }
+    
+    public  void netejar(){
+        try{
+            final int BLACK=0;
+            if (img == null) return;
+            MFSet api = new MFSet(img[0].length * img[1].length);
+    
+          for(int f = 0; f < img[0].length -1 ; f++) {
+              System.out.println(f + " / " + img[0].length);
+              for (int c  = 0; c < img[1].length - 1; c++) {
+                  System.out.println(c + " / " + img[1].length);
+                  if(img[f][c]==BLACK && img[f][c] >= 0){
+                      System.out.println(api.find(img[f][c]));
+                      if(f>0 && c>0){
+                      if(img[f-1][c+1] == BLACK){ 
+                          api.merge(img[f][c], img[f-1][c+1]);
+                        } // NordEst
+                      if(img[f][c+1] == BLACK) api.merge(img[f][c], img[f][c+1]); // Est
+                      if(img[f+1][c+1] == BLACK) api.merge(img[f][c], img[f+1][c+1]); // SudEst
+                      if(img[f+1][c] == BLACK) api.merge(img[f][c], img[f+1][c]); // Sud
+                      int size = api.findCardinalitat(img[f][c]) * -1; // Creo que el merge está fastidiado por esto todos deberían colgar del primero no?
+                      if(size < llindar) img[f][c] = 255;
+                    }
+                  }
+              }
+          }
+            }catch(Exception e){
+                System.out.println(e);
+            }
+    }
+
+
+    public int [][] getImage(){
+    return img;
+    }
+    
+}
+    
